b308ce34e9800557dfa638ba25874e4faf450e37
Andrea Jimenez <andrea.jimenez@unosquare.com>
2020-03-26 17:45:05 -0600

    Feature forchild

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes} from '@angular/router';
 
 // Containers
 import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component';
@@ -15,6 +16,13 @@ import { PassengerFormComponent } from './components/passenger-form/passenger-fo
 // service
 import { PassengerDashboardService } from './passenger-dashboard.service';
 
+const routes: Routes = [
+	{
+		path: 'passengers',
+		component: PassengerDashboardComponent
+	}
+]
+
 
 @NgModule ({
 	declarations: [
@@ -27,10 +35,8 @@ import { PassengerDashboardService } from './passenger-dashboard.service';
 	imports: [
 		CommonModule, 
 		HttpModule,
-		FormsModule
-	],
-	exports: [
-		PassengerViewerComponent
+		FormsModule,
+		RouterModule.forChild(routes)
 	],
 	providers: [
 		PassengerDashboardService
