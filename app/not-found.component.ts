42af6b53196ba504d056d100412073ad9f31a631
Andrea Jimenez <andrea.jimenez@unosquare.com>
2020-03-26 12:12:59 -0600

    RouterLink

--- app/not-found.component.ts
+++ app/not-found.component.ts
@@ -4,7 +4,8 @@ import { Component } from '@angular/core';
 	selector: 'not-found',
 	template:  `
 	<div>
-		Not found
-	</div>`
+		Not found, <a routerLink='/'>go home?</a>
+	</div>
+	`
 })
 export class NotFoundComponent {}
