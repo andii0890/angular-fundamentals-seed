92a88c8196771fc7bf0500fb0ffa3dc8343568d7
Andrea Jimenez <andrea.jimenez@unosquare.com>
2020-03-25 18:35:02 -0600

    ngSubmit Stateless Output

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input } from  '@angular/core';
+import { Component, Input, Output, EventEmitter } from  '@angular/core';
 import { Passenger } from '../../models/passenger.interface';
 import { Baggage } from '../../models/baggage.interface';
+
 @Component({
 	selector: 'passenger-form',
 	styleUrls: ['passenger-form.component.scss'],
 	template:`
-	<form #form='ngForm' novalidate> 
+	<form (ngSubmit)="handleSubmit(form.value, form.valid)" #form='ngForm' novalidate> 
 
 		<div>
 			Passenger name:
@@ -76,6 +77,9 @@ export class PassengerFormComponent {
 	@Input()
 	detail: Passenger;
 
+	@Output()
+	update: EventEmitter<Passenger> = new EventEmitter<Passenger>();
+
 	baggage: Baggage[] = [{
 		key: 'none',
 		value: 'No baggage'
@@ -92,7 +96,12 @@ export class PassengerFormComponent {
 
 	toggleCheckIn(checkedIn: boolean) {
 		if(checkedIn) {
-			this.detail.checkInDate = Date.now(); //ms
+			this.detail.checkInDate = Date.now();
+		}
+	}
+	handleSubmit(passenger: Passenger, isValid: boolean) {
+		if(isValid) {
+			this.update.emit(passenger);
 		}
 	}
 }
